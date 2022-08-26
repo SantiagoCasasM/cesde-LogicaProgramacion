f31c6e57e87d93a0f288c6619733edf884c6f420
Santiago Casas <50216041+SantiagoCasasM@users.noreply.github.com>
2022-08-26 08:48:19 -0500

    Add files via upload

---
+++ Clase1 Java/SCMJavaAgain/src/jcEjemplo1.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author 108
+ */
+public class jcEjemplo1 {
+    public static void main(String[ ]arg){ 
+ 
+       // int num1;
+       // int num2;
+       // int sumatoria;
+        double num3; 
+        double num4;
+        double resultado2;
+        float num5; 
+        float num6;
+        float resultado1;
+
+        
+        num5 = 4.454f;
+        num6 = 4.484f;
+        
+        resultado2 = num5 + num6;
+        
+       // num1 = 4;
+       // num2 = 5;
+        
+       // sumatoria = num1 + num2;
+        
+        System.out.println("El numero de la sumatoria es : " + resultado2);
+        
+    }
+}
---
+++ Clase1 Java/SCMJavaAgain/src/scmjavaagain/SCMJavaAgain.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package scmjavaagain;
+
+/**
+ *
+ * @author 108
+ */
+public class SCMJavaAgain {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    /** Inicio clase 26/08/2022 
+     * 
+     */
+    
+}
